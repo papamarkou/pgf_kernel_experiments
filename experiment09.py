fe7125aff6fc4227b166fa52568f4b890856a2d7
Theodore Papamarkou <theodore.papamarkou@gmail.com>
2023-01-28 17:55:58 +0000

    Some notes in experiment09

--- experiment09.py
+++ experiment09.py
@@ -1,5 +1,13 @@
 # %%
 
+# Data downloaded from this page:
+# https://data.giss.nasa.gov/gistemp/
+
+# Direct link to download the Zarr file:
+# https://data.giss.nasa.gov/pub/gistemp/gistemp1200_GHCNv4_ERSSTv5.zarr.tar.gz
+
+# %%
+
 import zarr
 
 # %%
