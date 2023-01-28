57ded0d97da665b8e55fd84ae317d808be17f962
Theodore Papamarkou <theodore.papamarkou@gmail.com>
2023-01-28 22:34:32 +0000

    Made nasa visualizations

--- dev_installation.md
+++ dev_installation.md
@@ -7,9 +7,9 @@ conda install -c conda-forge numpy pytorch-cpu torchvision gpytorch
 
 conda install -c conda-forge pandas scikit-learn matplotlib seaborn ipykernel
 
-conda install -c conda-forge zarr
+conda install -c conda-forge zarr cartopy gstools
 
-# conda install -c conda-forge cartopy geotiff python-geotiff gstools
+# conda install -c conda-forge geotiff python-geotiff iris iris-sample-data
 
 cd /home/theodore/opt/python/packages
 
--- experiment09.py
+++ experiment09.py
@@ -23,3 +23,38 @@ dataset['lon']
 dataset['tempanomaly']
 
 dataset['time']
+
+# %%
+
+# from pyproj import CRS
+
+# %%
+
+import numpy as np
+
+lon, lat = np.meshgrid(dataset['lon'], dataset['lat'])
+
+# %%
+
+import matplotlib.pyplot as plt
+
+plt.contourf(lon, lat, dataset['tempanomaly'][1693, :, :], cmap="RdBu_r")
+# plt.contourf(lon.transpose(), lat.transpose(), dataset['tempanomaly'][1693, :, :].transpose(), cmap="RdBu_r")
+plt.colorbar()
+
+# %%
+
+from cartopy import crs as ccrs
+
+projMoll = ccrs.Mollweide(central_longitude=0)
+
+fig = plt.figure(figsize=(11, 8.5))
+ax = plt.subplot(1, 1, 1, projection=projMoll)
+ax.coastlines()
+dataplot = ax.contourf(lon, lat, dataset['tempanomaly'][1695, :, :], cmap="RdBu_r", transform=ccrs.PlateCarree())
+plt.colorbar(dataplot, orientation='horizontal');
+
+# %%
+
+dataset['lat'].shape, dataset['lon'].shape, dataset['tempanomaly'].shape
+# ((90,), (180,), (1711, 90, 180))
