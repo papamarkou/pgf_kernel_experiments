4b443263797310be1ece92874c64fafd606f2db9
Theodore Papamarkou <theodore.papamarkou@gmail.com>
2023-01-30 09:11:23 +0000

    Working on air passenger example

--- pgf_kernel_experiments/experiments/air_passengers/tune.py
+++ pgf_kernel_experiments/experiments/air_passengers/tune.py
@@ -1,14 +1,21 @@
 # %% Import packages
 
+import gpytorch
+import torch
+
+import matplotlib.pyplot as plt
 import numpy as np
+
+from pgf_kernel_experiments.runners.exact_multi_gp_runner import ExactMultiGPRunner
+from pgfml.kernels import GFKernel
 
 # %% Load data
 
 dataset = np.loadtxt('passenger_numbers.csv')
 
-# %% Plot data
+dataset = dataset / dataset.max()
 
-import matplotlib.pyplot as plt
+# %% Plot data
 
 plt.plot(dataset)
 
@@ -16,7 +23,7 @@ plt.plot(dataset)
 
 n_samples = len(dataset)
 
-theta = np.linspace(0, 2*np.pi, num=n_samples, endpoint=False)
+theta = np.linspace(-np.pi, np.pi, num=n_samples, endpoint=False)
 
 x = np.cos(theta)
 
@@ -24,5 +31,79 @@ y = np.sin(theta)
 
 grid = np.stack((x, y))
 
-z = dataset
+# %% Generate training data
 
+n_train = 96
+
+train_ids = range(n_samples)[:n_train]
+
+train_pos = grid[:, train_ids]
+
+train_output = dataset[train_ids]
+
+# %% Generate test data
+
+test_ids = range(n_train, n_samples)
+
+n_test = n_samples - n_train
+
+test_pos = grid[:, test_ids]
+
+test_output = dataset[test_ids]
+
+# %% Plot training and test data
+
+plt.plot(train_output)
+
+plt.plot(range(n_train, n_samples), test_output)
+
+# %% Convert training and test data to PyTorch format
+
+train_x = torch.as_tensor(train_pos.T, dtype=torch.float32)
+train_y = torch.as_tensor(train_output.T, dtype=torch.float32)
+
+test_x = torch.as_tensor(test_pos.T, dtype=torch.float32)
+test_y = torch.as_tensor(test_output.T, dtype=torch.float32)
+
+# %% Set up ExactMultiGPRunner
+
+kernels = [
+    GFKernel(width=[20, 20, 20]),
+    gpytorch.kernels.RBFKernel()
+]
+
+runner = ExactMultiGPRunner.generator(train_x, train_y, kernels)
+
+# %% Configurate training setup for GP model
+
+optimizers = []
+
+for i in range(runner.num_gps()):
+    optimizers.append(torch.optim.SGD(runner.single_runners[i].model.parameters(), lr=0.1))
+
+n_iters = 5000
+
+# %% Train GP model to find optimal hyperparameters
+
+losses = runner.train(train_x, train_y, optimizers, n_iters)
+
+# %% Make predictions
+
+predictions = runner.test(test_x)
+
+# %% Compute error metrics
+
+for i in range(2):
+    print('Test MAE: {}'.format(gpytorch.metrics.mean_absolute_error(predictions[i], test_y)))
+    print('Test MSE: {}'.format(gpytorch.metrics.mean_absolute_error(predictions[i], test_y)))
+    print('Test LPD: {}'.format(-gpytorch.metrics.negative_log_predictive_density(predictions[i], test_y)))
+
+# %% Plot predictions
+
+plt.plot(range(n_train), train_output)
+
+plt.plot(range(n_train, n_samples), test_output)
+
+plt.plot(range(n_train, n_samples), predictions[0].mean.numpy())
+
+plt.plot(range(n_train, n_samples), predictions[1].mean.numpy())
