996cd50e6f792cb26ff2d201bd134112f04c4001
Theodore Papamarkou <theodore.papamarkou@gmail.com>
2023-02-03 02:59:16 +0000

    Changed a seed

--- pgf_kernel_experiments/experiments/fourier_series/setting01/tune_multiple_gps.py
+++ pgf_kernel_experiments/experiments/fourier_series/setting01/tune_multiple_gps.py
@@ -50,7 +50,7 @@ ids = np.arange(n_samples)
 
 n_train = int(0.7 * n_samples)
 
-train_ids = np.random.RandomState(1).choice(ids, size=n_train, replace=False)
+train_ids = np.random.RandomState(2).choice(ids, size=n_train, replace=False)
 
 train_pos = grid[:, train_ids]
 
