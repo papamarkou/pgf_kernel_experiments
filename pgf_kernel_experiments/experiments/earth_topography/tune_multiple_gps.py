efe436c1fa2e13b1222ca8085fa0c9d7aa270d20
Theodore Papamarkou <theodore.papamarkou@gmail.com>
2023-02-03 19:58:12 +0000

    Half way with earth topography

--- pgf_kernel_experiments/experiments/earth_topography/tune_multiple_gps.py
+++ pgf_kernel_experiments/experiments/earth_topography/tune_multiple_gps.py
@@ -19,6 +19,19 @@ geo_tiff = GeoTiff("sea_bed.tiff")
 
 rgb_dataset = np.array(geo_tiff.read())
 
+# %% Set up latitude-longitude grid
+
+n_lat, n_lon, _ = rgb_dataset.shape
+
+lon_left = 18.5
+lon_right = 21.5
+
+lat_left = 34.0
+lat_right = 37.0
+
+lon = np.linspace(lon_left, lon_right, num=n_lat)
+lat = np.linspace(lat_left, lat_right, num=n_lon)
+
 # %% Convert image from RGB to grayscale
 
 gray_dataset = rgb2gray(rgb_dataset)
@@ -27,7 +40,11 @@ gray_dataset = rgb2gray(rgb_dataset)
 
 fontsize = 11
 
-fig, ax = plt.subplots(1, 4, figsize=[14, 3])
+fig, ax = plt.subplots(1, 4, figsize=[14, 3], sharey=True)
+
+#  https://www.geeksforgeeks.org/how-to-set-the-spacing-between-subplots-in-matplotlib-in-python/
+
+plt.subplots_adjust(wspace=0.2)
 
 ax[0].imshow(rgb_dataset)
 ax[1].imshow(gray_dataset, cmap=plt.get_cmap('gray'))
@@ -37,4 +54,22 @@ ax[1].set_title(r'$Grayscale~image$', fontsize=fontsize)
 ax[2].set_title(r'$Training~data$', fontsize=fontsize)
 ax[3].set_title(r'$Test~data$', fontsize=fontsize)
 
+xtick_ticks = np.linspace(0, 720, num=3)
+xtick_labels = np.linspace(lon_left, lon_right, num=3)
+
+ytick_ticks = np.linspace(0, 720, num=3)
+ytick_labels = np.flip(np.linspace(lat_left, lat_right, num=3))
+
+ax[0].set_xticks(ticks=xtick_ticks, labels=xtick_labels, fontsize=fontsize)
+ax[1].set_xticks(ticks=xtick_ticks, labels=xtick_labels, fontsize=fontsize)
+ax[2].set_xticks(ticks=xtick_ticks, labels=xtick_labels, fontsize=fontsize)
+ax[3].set_xticks(ticks=xtick_ticks, labels=xtick_labels, fontsize=fontsize)
+
+ax[0].set_yticks(ticks=ytick_ticks, labels=ytick_labels, fontsize=fontsize)
+ax[1].set_yticks(ticks=ytick_ticks, labels=ytick_labels, fontsize=fontsize)
+ax[2].set_yticks(ticks=ytick_ticks, labels=ytick_labels, fontsize=fontsize)
+ax[3].set_yticks(ticks=ytick_ticks, labels=ytick_labels, fontsize=fontsize)
+
 # %%
+
+np.meshgrid(lat, np.flip(lon))
