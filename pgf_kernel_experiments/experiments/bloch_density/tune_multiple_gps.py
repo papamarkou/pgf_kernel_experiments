c21660907d18756cc9c8b19033738b3f2e591709
Theodore Papamarkou <theodore.papamarkou@gmail.com>
2023-02-11 11:33:05 +0000

    Tune multiple gps script in bloch density

--- pgf_kernel_experiments/experiments/bloch_density/tune_multiple_gps.py
+++ pgf_kernel_experiments/experiments/bloch_density/tune_multiple_gps.py
@@ -1,21 +1,27 @@
 # %% Import packages
 
+import gpytorch
+import torch
+
 import matplotlib.pyplot as plt
 import numpy as np
 
-from bloch_density import BlochDensity
-from set_paths import data_path
+from pgf_kernel_experiments.runners import ExactMultiGPRunner
+from pgfml.kernels import GFKernel
+
+# from bloch_density import BlochDensity
+from pgf_kernel_experiments.experiments.bloch_density.set_paths import data_path
 
 # %% Load data
 
 phi = np.loadtxt(data_path.joinpath('phi.csv'))
 theta = np.loadtxt(data_path.joinpath('theta.csv'))
 
-x = np.loadtxt(data_path.joinpath('x.csv'), delimiter=',')
-y = np.loadtxt(data_path.joinpath('y.csv'), delimiter=',')
-z = np.loadtxt(data_path.joinpath('z.csv'), delimiter=',')
+x = np.loadtxt(data_path.joinpath('x.csv'), delimiter=',')[:-1, :]
+y = np.loadtxt(data_path.joinpath('y.csv'), delimiter=',')[:-1, :]
+z = np.loadtxt(data_path.joinpath('z.csv'), delimiter=',')[:-1, :]
 
-freqs = np.loadtxt(data_path.joinpath('freqs.csv'), delimiter=',')
+freqs = np.loadtxt(data_path.joinpath('freqs.csv'), delimiter=',')[:-1, :]
 
 train_ids = np.loadtxt(data_path.joinpath('train_ids.csv'), dtype='int')
 test_ids = np.loadtxt(data_path.joinpath('test_ids.csv'), dtype='int')
@@ -29,6 +35,14 @@ freqs_flat = freqs.flatten()
 pos = np.column_stack((x_flat, y_flat, z_flat))
 
 # %% Set up training and test data
+
+train_pos = pos[train_ids, :]
+
+train_output = freqs_flat[train_ids]
+
+test_pos = pos[test_ids, :]
+
+test_output = freqs_flat[test_ids]
 
 # %% Generate BlochDensity for all data
 
@@ -121,5 +135,60 @@ ax3.set_box_aspect([1, 1, 1])
 ax3.set_title('Test data', fontsize=fontsize)
 
 # plt.show()
+
+# %% Convert training and test data to PyTorch format
+
+train_x = torch.as_tensor(train_pos, dtype=torch.float64)
+train_y = torch.as_tensor(train_output, dtype=torch.float64)
+
+test_x = torch.as_tensor(test_pos, dtype=torch.float64)
+test_y = torch.as_tensor(test_output, dtype=torch.float64)
+
+# %% Set up ExactMultiGPRunner
+
+kernels = [
+    GFKernel(width=[20, 20, 20]),
+    gpytorch.kernels.ScaleKernel(gpytorch.kernels.RBFKernel()),
+    gpytorch.kernels.ScaleKernel(gpytorch.kernels.MaternKernel(nu=0.5)),
+    gpytorch.kernels.PeriodicKernel(),
+    gpytorch.kernels.SpectralMixtureKernel(num_mixtures=10, ard_num_dims=3)
+]
+
+runner = ExactMultiGPRunner.generator(train_x, train_y, kernels)
+
+# %% Set the models in double mode
+
+for i in range(len(kernels)):
+    runner.single_runners[i].model.double()
+    runner.single_runners[i].model.likelihood.double()
+
+# %% Configurate training setup for GP models
+
+optimizers = []
+
+for i in range(runner.num_gps()):
+    optimizers.append(torch.optim.Adam(runner.single_runners[i].model.parameters(), lr=0.1))
+
+n_iters = 10
+
+# %% Train GP models to find optimal hyperparameters
+
+losses = runner.train(train_x, train_y, optimizers, n_iters)
+
+# %% Make predictions
+
+predictions = runner.test(test_x)
+
+# %% Compute error metrics
+
+scores = runner.assess(
+    predictions,
+    test_y,
+    metrics=[
+        gpytorch.metrics.mean_absolute_error,
+        gpytorch.metrics.mean_squared_error,
+        lambda predictions, y : -gpytorch.metrics.negative_log_predictive_density(predictions, y)
+    ]
+)
 
 # %%
