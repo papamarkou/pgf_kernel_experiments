d1ef4e912c1483e5767e2ec8c613f9793b2db926
Theodore Papamarkou <theodore.papamarkou@gmail.com>
2023-07-30 17:11:30 +0100

    Finalized first draft of fourier series kernel comparison setting01

--- pgf_kernel_experiments/experiments/fourier_series/kernel_comparison/setting01/run_numerics.sh
+++ pgf_kernel_experiments/experiments/fourier_series/kernel_comparison/setting01/run_numerics.sh
@@ -4,6 +4,9 @@ declare -a files=(
     "simulate_data.sh"
     "train_set_run_numerics.sh"
     "train_subset4_run_numerics.sh"
+    "train_subset3_run_numerics.sh"
+    "train_subset2_run_numerics.sh"
+    "train_subset1_run_numerics.sh"
 )
 
 for file in "${files[@]}"
