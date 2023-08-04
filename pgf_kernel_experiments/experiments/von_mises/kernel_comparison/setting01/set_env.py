4927709899c0c19f75072e1a6831688e29c724d6
Theodore Papamarkou <theodore.papamarkou@gmail.com>
2023-08-04 22:40:00 +0000

    Script for 10 datasets in von mises kernel comparison setting01

--- pgf_kernel_experiments/experiments/von_mises/kernel_comparison/setting01/set_env.py
+++ pgf_kernel_experiments/experiments/von_mises/kernel_comparison/setting01/set_env.py
@@ -2,11 +2,25 @@
 
 from pathlib import Path
 
-# %% Set paths
-
-data_path = Path('data')
-output_path = Path('output')
-
 # %% Set seed
 
-seed = 1
+data_seed = 1
+train_seed = 100000
+
+# %% Set number of runs
+
+num_runs = 10
+
+# %% Data simulation setup
+
+num_samples = 1000
+
+perc_training = 0.5
+
+# %% Set paths
+
+data_basepath = Path('data')
+
+data_paths = [Path(data_basepath, 'run'+str(i+1).zfill(len(str(num_runs)))) for i in range(num_runs)]
+
+output_path = Path('output')
