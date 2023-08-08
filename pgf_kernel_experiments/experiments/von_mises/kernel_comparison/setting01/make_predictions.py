de4452c60af7c6028d26fd7808098612bd5786d2
Theodore Papamarkou <theodore.papamarkou@gmail.com>
2023-08-08 19:50:47 +0100

    Drafted template for making predictions in kernel comparison many times (von_mises)

--- pgf_kernel_experiments/experiments/von_mises/kernel_comparison/setting01/make_predictions.py
+++ pgf_kernel_experiments/experiments/von_mises/kernel_comparison/setting01/make_predictions.py
@@ -6,107 +6,128 @@ import torch
 
 from pgfml.kernels import GFKernel
 
-from pgf_kernel_experiments.experiments.von_mises.kernel_comparison.setting01.set_env import data_path, output_path
+from pgf_kernel_experiments.experiments.von_mises.kernel_comparison.setting01.set_env import (
+    data_paths, num_runs, output_paths, use_cuda
+)
 from pgf_kernel_experiments.runners import ExactMultiGPRunner
 
-# %% Create paths if they don't exist
+# %% Make and save predictions
 
-output_path.mkdir(parents=True, exist_ok=True)
+verbose = True
+if verbose:
+    num_run_digits = len(str(num_runs))
+    msg = 'Run {:'+str(num_run_digits)+'d}/{:'+str(num_run_digits)+'d} completed'
 
-# %% Load data
+for run_count in range(num_runs):
+    # Load data
 
-data = np.loadtxt(
-    data_path.joinpath('data.csv'),
-    delimiter=',',
-    skiprows=1
-)
+    data = np.loadtxt(
+        data_paths[run_count].joinpath('data.csv'),
+        delimiter=',',
+        skiprows=1
+    )
 
-grid = data[:, 1:3]
-x = data[:, 1]
-y = data[:, 2]
-z = data[:, 3]
+    grid = data[:, 1:3]
+    x = data[:, 1]
+    y = data[:, 2]
+    z = data[:, 3]
 
-train_ids = np.loadtxt(data_path.joinpath('train_ids.csv'), dtype='int')
+    train_ids = np.loadtxt(data_paths[run_count].joinpath('train_ids.csv'), dtype='int')
 
-test_ids = np.loadtxt(data_path.joinpath('test_ids.csv'), dtype='int')
+    test_ids = np.loadtxt(data_paths[run_count].joinpath('test_ids.csv'), dtype='int')
 
-# %% Get training data
+    # Get training data
 
-train_pos = grid[train_ids, :]
-train_output = z[train_ids]
+    train_pos = grid[train_ids, :]
+    train_output = z[train_ids]
 
-# %% Get test data
+    # Get test data
 
-test_pos = grid[test_ids, :]
-test_output = z[test_ids]
+    test_pos = grid[test_ids, :]
+    test_output = z[test_ids]
 
-# %% Convert training data to PyTorch format
+    # Convert training data to PyTorch format
 
-train_x = torch.as_tensor(train_pos, dtype=torch.float64)
-train_y = torch.as_tensor(train_output.T, dtype=torch.float64)
+    train_x = torch.as_tensor(train_pos, dtype=torch.float64)
+    train_y = torch.as_tensor(train_output.T, dtype=torch.float64)
 
-# %% Convert test data to PyTorch format
+    if use_cuda:
+        train_x = train_x.cuda()
+        train_y = train_y.cuda()
 
-test_x = torch.as_tensor(test_pos, dtype=torch.float64)
-test_y = torch.as_tensor(test_output.T, dtype=torch.float64)
+    # Convert test data to PyTorch format
 
-# %% Set up ExactMultiGPRunner
+    test_x = torch.as_tensor(test_pos, dtype=torch.float64)
+    test_y = torch.as_tensor(test_output.T, dtype=torch.float64)
 
-kernels = [
-    GFKernel(width=[30, 30, 30]),
-    gpytorch.kernels.ScaleKernel(gpytorch.kernels.RBFKernel()),
-    gpytorch.kernels.ScaleKernel(gpytorch.kernels.MaternKernel(nu=0.5)),
-    gpytorch.kernels.PeriodicKernel(),
-    gpytorch.kernels.SpectralMixtureKernel(num_mixtures=10, ard_num_dims=2)
-]
+    if use_cuda:
+        test_x = test_x.cuda()
+        test_y = test_y.cuda()
 
-kernel_names = ['pgf', 'rbf', 'matern', 'periodic', 'spectral']
+    # Set up ExactMultiGPRunner
 
-runner = ExactMultiGPRunner.generator(train_x, train_y, kernels)
-
-# %% Set the models in double mode
-
-for i in range(runner.num_gps()):
-    runner.single_runners[i].model.double()
-    runner.single_runners[i].model.likelihood.double()
-
-# %% Load model states
-
-for i in range(runner.num_gps()):
-    runner.single_runners[i].model.load_state_dict(torch.load(output_path.joinpath(kernel_names[i]+'_gp_state.pth')))
-
-# %% Make predictions
-
-predictions = runner.test(test_x)
-
-# %% Compute error metrics
-
-scores = runner.assess(
-    predictions,
-    test_y,
-    metrics=[
-        gpytorch.metrics.mean_absolute_error,
-        gpytorch.metrics.mean_squared_error,
-        lambda predictions, y : gpytorch.metrics.negative_log_predictive_density(predictions, y)
+    kernels = [
+        GFKernel(width=[30, 30, 30]),
+        gpytorch.kernels.ScaleKernel(gpytorch.kernels.RBFKernel()),
+        gpytorch.kernels.ScaleKernel(gpytorch.kernels.MaternKernel(nu=0.5)),
+        gpytorch.kernels.PeriodicKernel(),
+        gpytorch.kernels.SpectralMixtureKernel(num_mixtures=10, ard_num_dims=2)
     ]
-)
 
-# %% Save predictions
+    kernel_names = ['pgf', 'rbf', 'matern', 'periodic', 'spectral']
 
-np.savetxt(
-    output_path.joinpath('predictions.csv'),
-    torch.stack([predictions[i].mean for i in range(runner.num_gps())], dim=0).t().detach().numpy(),
-    delimiter=',',
-    header=','.join(kernel_names),
-    comments=''
-)
+    runner = ExactMultiGPRunner.generator(train_x, train_y, kernels, use_cuda=use_cuda)
 
-# %% Save error metrics
+    # Set the models in double mode
 
-np.savetxt(
-    output_path.joinpath('error_metrics.csv'),
-    scores.detach().numpy(),
-    delimiter=',',
-    header='mean_abs_error,mean_sq_error,loss',
-    comments=''
-)
+    for i in range(runner.num_gps()):
+        runner.single_runners[i].model.double()
+        runner.single_runners[i].model.likelihood.double()
+
+    # Load model states
+
+    for i in range(runner.num_gps()):
+        runner.single_runners[i].model.load_state_dict(
+            torch.load(output_paths[run_count].joinpath(kernel_names[i]+'_gp_state.pth'))
+        )
+
+    # Make predictions
+
+    predictions = runner.test(test_x)
+
+    # Compute error metrics
+
+    scores = runner.assess(
+        predictions,
+        test_y,
+        metrics=[
+            gpytorch.metrics.mean_absolute_error,
+            gpytorch.metrics.mean_squared_error,
+            lambda predictions, y : gpytorch.metrics.negative_log_predictive_density(predictions, y)
+        ]
+    )
+
+    # Save predictions
+
+    np.savetxt(
+        output_paths[run_count].joinpath('predictions.csv'),
+        torch.stack([predictions[i].mean for i in range(runner.num_gps())], dim=0).t().cpu().detach().numpy(),
+        delimiter=',',
+        header=','.join(kernel_names),
+        comments=''
+    )
+
+    # Save error metrics
+
+    np.savetxt(
+        output_paths[run_count].joinpath('error_metrics.csv'),
+        scores.cpu().detach().numpy(),
+        delimiter=',',
+        header='mean_abs_error,mean_sq_error,loss',
+        comments=''
+    )
+
+    # If verbose, state run number
+
+    if verbose:
+        print(msg.format(run_count+1, num_runs))
