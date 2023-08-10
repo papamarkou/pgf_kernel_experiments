3eb10cdddd8d0808beb29061773442e88f090657
Theodore Papamarkou <theodore.papamarkou@gmail.com>
2023-08-10 23:40:27 +0100

    Nearly done multirun plotting of losses in von mises kernel comparison setting01

--- pgf_kernel_experiments/experiments/von_mises/kernel_comparison/setting01/plot_losses.py
+++ pgf_kernel_experiments/experiments/von_mises/kernel_comparison/setting01/plot_losses.py
@@ -3,19 +3,18 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
-from pgf_kernel_experiments.experiments.von_mises.kernel_comparison.setting01.set_env import output_path
+from pgf_kernel_experiments.experiments.von_mises.kernel_comparison.setting01.set_env import num_runs, output_paths
 
-# %% Load predictions
+# %% Generate and save plots of data
 
-losses = np.loadtxt(
-    output_path.joinpath('losses.csv'),
-    delimiter=',',
-    skiprows=1
-)
+verbose = True
+if verbose:
+    num_run_digits = len(str(num_runs))
+    msg = 'Plotting losses of run {:'+str(num_run_digits)+'d}/{:'+str(num_run_digits)+'d}'
 
-num_iters, num_kernels = losses.shape
-
-# %% Plot losses
+manual_ylim = True
+if manual_ylim:
+    manual_ylims = [[-2, 10] for _ in range(num_runs)]
 
 labels = ['PGF kernel','RBF kernel', 'Matern kernel', 'Periodic kernel', 'Spectral kernel']
 
@@ -23,43 +22,63 @@ label_fontsize = 11
 axis_fontsize = 11
 legend_fontsize = 11
 
-plt.figure(figsize=[7, 4])
+for i in range(num_runs):
+    # If verbose, state run number
 
-plt.margins(0.)
+    if verbose:
+        print(msg.format(i+1, num_runs))
 
-plt.xlabel('Iteration', fontsize=label_fontsize)
-plt.ylabel('Loss', fontsize=label_fontsize)
+    # Load losses
 
-plt.xlim([1, num_iters+1])
-# plt.ylim([losses.min(), losses.max()])
-plt.ylim([-2., 10])
+    losses = np.loadtxt(
+        output_paths[i].joinpath('losses.csv'),
+        delimiter=',',
+        skiprows=1
+    )
 
-handles = []
-for i in range(num_kernels):
-    handle, = plt.plot(range(1, num_iters+1), losses[:, i])
-    handles.append(handle)
+    num_iters, num_kernels = losses.shape
 
-plt.xticks(np.arange(0, num_iters+50, 50), fontsize=axis_fontsize)
-plt.yticks(np.arange(-2, 10+2, 2), fontsize=axis_fontsize)
+    # Plot losses
 
-plt.legend(
-    handles,
-    labels,
-    ncol=1,
-    frameon=False,
-    markerscale=2,
-    fontsize=legend_fontsize
-)
+    plt.figure(figsize=[7, 4])
 
-# %% Save plot
+    plt.margins(0.)
 
-plt.savefig(
-    output_path.joinpath('losses.png'),
-    dpi=600,
-    pil_kwargs={'quality': 100},
-    transparent=True,
-    bbox_inches='tight',
-    pad_inches=0.1
-)
+    plt.xlim([1, num_iters+1])
+    if manual_ylim:
+        plt.ylim(manual_ylims[i])
+    else:
+        plt.ylim([losses.min(), losses.max()])
 
-plt.close()
+    plt.xlabel('Iteration', fontsize=label_fontsize)
+    plt.ylabel('Loss', fontsize=label_fontsize)
+
+    handles = []
+    for j in range(num_kernels):
+        handle, = plt.plot(range(1, num_iters+1), losses[:, j])
+        handles.append(handle)
+
+    plt.xticks(np.arange(0, num_iters+50, 50), fontsize=axis_fontsize)
+    plt.yticks(np.arange(-2, 10+2, 2), fontsize=axis_fontsize)
+
+    plt.legend(
+        handles,
+        labels,
+        ncol=1,
+        frameon=False,
+        markerscale=2,
+        fontsize=legend_fontsize
+    )
+
+    # Save plot
+
+    plt.savefig(
+        output_paths[i].joinpath('losses.png'),
+        dpi=600,
+        pil_kwargs={'quality': 100},
+        transparent=True,
+        bbox_inches='tight',
+        pad_inches=0.1
+    )
+
+    plt.close()
