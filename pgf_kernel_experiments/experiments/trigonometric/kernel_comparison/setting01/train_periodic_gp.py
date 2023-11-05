39124109068e674d9f22d7163a7de295ebda1641
Theodore Papamarkou <theodore.papamarkou@gmail.com>
2023-11-05 10:39:53 +0000

    Tuned periodic GP in trigonometric

--- pgf_kernel_experiments/experiments/trigonometric/kernel_comparison/setting01/train_periodic_gp.py
+++ pgf_kernel_experiments/experiments/trigonometric/kernel_comparison/setting01/train_periodic_gp.py
@@ -79,30 +79,20 @@ while ((success_count < num_runs) and (tot_count < num_train_seeds)):
         optimizer = torch.optim.Adam([
             {"params": runner.model.likelihood.noise_covar.raw_noise, "lr": 0.1},
             {"params": runner.model.mean_module.raw_constant, "lr": 0.1},
-            {"params": runner.model.covar_module.raw_lengthscale, "lr": 0.075},
-            {"params": runner.model.covar_module.raw_period_length, "lr": 0.075}
+            {"params": runner.model.covar_module.raw_lengthscale, "lr": 0.1},
+            {"params": runner.model.covar_module.raw_period_length, "lr": 0.1}
         ])
 
         # Set scheduler
 
         scheduler = torch.optim.lr_scheduler.CyclicLR(
             optimizer,
-            base_lr=[0.05, 0.05, 0.05, 0.05],
-            # max_lr=[0.1, 0.1, 0.075, 0.075],
-            max_lr=[0.1, 0.1, 0.5, 0.5],
+            base_lr=[0.01, 0.01, 0.01, 0.01],
+            max_lr=[0.1, 0.1, 0.1, 0.1],
             step_size_up=25,
-            scale_fn=lambda x : 0.9 ** (x - 1), 
+            scale_fn=lambda x : 0.97 ** (x - 1),
             cycle_momentum=False
         )
-        # scheduler = torch.optim.lr_scheduler.CyclicLR(
-        #     optimizer,
-        #     base_lr=[0.05, 0.05, 0.05, 0.05],
-        #     max_lr=[0.1, 0.1, 0.075, 0.075],
-        #     step_size_up=25,
-        #     mode='triangular',
-        #     cycle_momentum=False
-        # )
-        # scheduler = torch.optim.lr_scheduler.CosineAnnealingWarmRestarts(optimizer, T_0=50, T_mult=1, eta_min=0.05)
 
         # Train GP model to find optimal hyperparameters
 
