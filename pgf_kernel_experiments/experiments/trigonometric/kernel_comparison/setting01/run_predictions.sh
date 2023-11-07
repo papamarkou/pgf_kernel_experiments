11a04535ddfbb3a3ed386cf887f000af1c8d7d0a
Theodore Papamarkou <theodore.papamarkou@gmail.com>
2023-11-07 15:19:13 +0000

    Updated shell script for running predictions for trigonometric

--- pgf_kernel_experiments/experiments/trigonometric/kernel_comparison/setting01/run_predictions.sh
+++ pgf_kernel_experiments/experiments/trigonometric/kernel_comparison/setting01/run_predictions.sh
@@ -1,6 +1,15 @@
 #!/bin/bash
 
-file="make_predictions.py"
+declare -a files=(
+    "make_pgf_gp_predictions.py"
+    "make_rbf_gp_predictions.py"
+    "make_matern_gp_predictions.py"
+    "make_periodic_gp_predictions.py"
+    "make_spectral_gp_predictions.py"
+)
 
-echo "Executing $file..."
-python $file
+for file in "${files[@]}"
+do
+   echo "Executing $file..."
+   python $file
+done
