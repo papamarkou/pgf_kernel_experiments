bf22f55bf273eadb2a4b421e63aaccf078f70ee9
Theodore Papamarkou <theodore.papamarkou@gmail.com>
2023-11-07 18:06:09 +0000

    Started script for merging predictions in trigonometric

--- pgf_kernel_experiments/experiments/trigonometric/kernel_comparison/setting01/merge_predictions.py
+++ pgf_kernel_experiments/experiments/trigonometric/kernel_comparison/setting01/merge_predictions.py
@@ -1,1 +1,11 @@
-# Placeholder for merging predictions across kernels into a single file
+# %% Import packages
+
+import numpy as np
+
+from pgf_kernel_experiments.experiments.trigonometric.kernel_comparison.setting01.set_env import num_runs, output_paths
+
+# %% Load predictions based on different kernels
+
+kernel_names = ['pgf', 'rbf', 'matern', 'periodic', 'spectral']
+
+# %% Merge and save predictions into a single file
