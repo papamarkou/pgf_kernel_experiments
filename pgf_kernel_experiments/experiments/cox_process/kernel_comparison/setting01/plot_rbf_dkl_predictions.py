fe46d62cdc49f550bc73b596b0a8694337c3fc12
papamarkou <theodore.papamarkou@gmail.com>
2023-12-17 16:32:30 +0000

    wip on projections plot

--- pgf_kernel_experiments/experiments/cox_process/kernel_comparison/setting01/plot_rbf_dkl_predictions.py
+++ pgf_kernel_experiments/experiments/cox_process/kernel_comparison/setting01/plot_rbf_dkl_predictions.py
@@ -31,15 +31,27 @@ labels = labels[train_ids]
 
 projected_x = np.loadtxt(output_paths[i].joinpath('rbf_dkl_projections.csv'), delimiter=',')
 
-# %%
+# %% Generate data that determine the spherical surface
+
+# https://stackoverflow.com/questions/31768031/plotting-points-on-the-surface-of-a-sphere
+
+surface = {'r' : 1.}
+surface['phi'], surface['theta'] = np.mgrid[0.0:np.pi:100j, 0.0:2.0*np.pi:100j]
+surface['x'] = surface['r'] * np.sin(surface['phi']) * np.cos(surface['theta'])
+surface['y'] = surface['r'] * np.sin(surface['phi']) * np.sin(surface['theta'])
+surface['z'] = surface['r'] * np.cos(surface['phi'])
+
+# %% Plot projections
+
+# https://stackoverflow.com/questions/31768031/plotting-points-on-the-surface-of-a-sphere
 
 fig = plt.figure()
 ax = fig.add_subplot(111, projection='3d')
 
 ax.plot_surface(
-    projected_x[:, 0],
-    projected_x[:, 1],
-    projected_x[:, 2],
+    surface['x'],
+    surface['y'],
+    surface['z'],
     rstride=1,
     cstride=1,
     color='c',
@@ -47,48 +59,13 @@ ax.plot_surface(
     linewidth=0
 )
 
-# %%
-
-# https://stackoverflow.com/questions/31768031/plotting-points-on-the-surface-of-a-sphere
-
-import matplotlib.pyplot as plt
-from matplotlib import cm, colors
-from mpl_toolkits.mplot3d import Axes3D
-import numpy as np
-
-# %%
-
-# Create a sphere
-r = 1
-pi = np.pi
-cos = np.cos
-sin = np.sin
-phi, theta = np.mgrid[0.0:pi:100j, 0.0:2.0*pi:100j]
-x = r*sin(phi)*cos(theta)
-y = r*sin(phi)*sin(theta)
-z = r*cos(phi)
-
-# #Import data
-# data = np.genfromtxt('leb.txt')
-# theta, phi, r = np.hsplit(data, 3) 
-# theta = theta * pi / 180.0
-# phi = phi * pi / 180.0
-# xx = sin(phi)*cos(theta)
-# yy = sin(phi)*sin(theta)
-# zz = cos(phi)
-
-xx = projected_x[:, 0]
-yy = projected_x[:, 1]
-zz = projected_x[:, 2]
-
-#Set colours and render
-fig = plt.figure()
-ax = fig.add_subplot(111, projection='3d')
-
-ax.plot_surface(
-    x, y, z,  rstride=1, cstride=1, color='c', alpha=0.3, linewidth=0)
-
-ax.scatter(xx,yy,zz,color="k",s=20)
+ax.scatter(
+    projected_x[:, 0],
+    projected_x[:, 1],
+    projected_x[:, 2],
+    color="k",
+    s=20
+)
 
 ax.set_xlim([-1,1])
 ax.set_ylim([-1,1])
