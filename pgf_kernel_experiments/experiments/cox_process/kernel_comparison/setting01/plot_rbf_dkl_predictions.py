15b50c3dcd45921ee08fcd1c4c32e1f7b6f99773
papamarkou <theodore.papamarkou@gmail.com>
2023-12-17 17:03:04 +0000

    Finished first draft of plot for rbf dkl predictions (apart from color)

--- pgf_kernel_experiments/experiments/cox_process/kernel_comparison/setting01/plot_rbf_dkl_predictions.py
+++ pgf_kernel_experiments/experiments/cox_process/kernel_comparison/setting01/plot_rbf_dkl_predictions.py
@@ -4,7 +4,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 from pgf_kernel_experiments.experiments.cox_process.kernel_comparison.setting01.set_env import (
-    data_paths, num_runs, output_paths
+    data_paths, dpi, num_runs, output_paths
 )
 
 # %% Generate and save plots of predictions with RBF kernel
@@ -14,22 +14,7 @@ if verbose:
     num_run_digits = len(str(num_runs))
     msg = 'Plotting predictions of run {:'+str(num_run_digits)+'d}/{:'+str(num_run_digits)+'d}...'
 
-# title_fontsize = 15
-axis_fontsize = 11
-
-# %%
-
-i = 0
-
-# Load labels
-
-labels = np.loadtxt(data_paths[i].joinpath('labels.csv'), dtype='int')
-train_ids = np.loadtxt(data_paths[i].joinpath('train_ids.csv'), dtype='int')
-labels = labels[train_ids]
-
-# Load projections
-
-projected_x = np.loadtxt(output_paths[i].joinpath('rbf_dkl_projections.csv'), delimiter=',')
+xyz_lim = 0.63
 
 # %% Generate data that determine the spherical surface
 
@@ -41,35 +26,76 @@ surface['x'] = surface['r'] * np.sin(surface['phi']) * np.cos(surface['theta'])
 surface['y'] = surface['r'] * np.sin(surface['phi']) * np.sin(surface['theta'])
 surface['z'] = surface['r'] * np.cos(surface['phi'])
 
-# %% Plot projections
+# %% Generate and save plots of projections
 
 # https://stackoverflow.com/questions/31768031/plotting-points-on-the-surface-of-a-sphere
 
-fig = plt.figure()
-ax = fig.add_subplot(111, projection='3d')
+for i in range(num_runs):
+    # If verbose, state run number
 
-ax.plot_surface(
-    surface['x'],
-    surface['y'],
-    surface['z'],
-    rstride=1,
-    cstride=1,
-    color='c',
-    alpha=0.3,
-    linewidth=0
-)
+    if verbose:
+        print(msg.format(i+1, num_runs))
 
-ax.scatter(
-    projected_x[:, 0],
-    projected_x[:, 1],
-    projected_x[:, 2],
-    color="k",
-    s=20
-)
+    # Load labels
 
-ax.set_xlim([-1,1])
-ax.set_ylim([-1,1])
-ax.set_zlim([-1,1])
-ax.set_aspect("equal")
+    labels = np.loadtxt(data_paths[i].joinpath('labels.csv'), dtype='int')
+    train_ids = np.loadtxt(data_paths[i].joinpath('train_ids.csv'), dtype='int')
+    labels = labels[train_ids]
+
+    # Load projections
+
+    projected_x = np.loadtxt(output_paths[i].joinpath('rbf_dkl_projections.csv'), delimiter=',')
+
+    # Set up figure
+
+    fig = plt.figure()
+
+    ax = fig.add_subplot(111, projection='3d')
+
+    # Plot spherical surface
+
+    ax.plot_surface(
+        surface['x'],
+        surface['y'],
+        surface['z'],
+        cstride=1,
+        rstride=1,
+        edgecolor='none',
+        alpha=0.25
+    )
+
+    # Plot projections
+
+    ax.scatter(
+        projected_x[:, 0],
+        projected_x[:, 1],
+        projected_x[:, 2],
+        color="k",
+        s=20
+    )
+
+    # Configure plot
+
+    ax.set_box_aspect([1, 1, 1])
+
+    ax.set_aspect("equal")
+
+    ax.grid(False)
+    ax.axis('off')
+
+    ax.set_xlim(-xyz_lim, xyz_lim)
+    ax.set_ylim(-xyz_lim, xyz_lim)
+    ax.set_zlim(-xyz_lim, xyz_lim)
+
+    # Save plot
+
+    plt.savefig(
+        output_paths[i].joinpath('rbf_dkl_predictions.pdf'),
+        dpi=dpi,
+        bbox_inches='tight',
+        pad_inches=0.1
+    )
+
+    plt.close(fig)
 
 # %%
